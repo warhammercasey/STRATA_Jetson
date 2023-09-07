e765b48ce0f60d1d973f2742b8853cbf507932c3
warhammercasey <warhammercasey@gmail.com>
2023-09-07 13:55:19 -0600

    Created catkin workspace

---
+++ catkin/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/strata/git/STRATA_Jetson/catkin/src
+# Build directory: /home/strata/git/STRATA_Jetson/catkin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ catkin/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/strata/git/STRATA_Jetson/catkin/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/strata/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/strata/git/STRATA_Jetson/catkin/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/strata/git/STRATA_Jetson/catkin/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/strata/git/STRATA_Jetson/catkin/src:$ROS_PACKAGE_PATH"
