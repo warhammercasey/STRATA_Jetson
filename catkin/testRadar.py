6fe1c4d650f16e18bd4b2b9734eae60356cd2042
Peter Manfredo <pmanfredo@mines.edu>
2023-09-26 10:33:31 -0600

    will this ever work

--- catkin/testRadar.py
+++ catkin/testRadar.py
@@ -1,6 +1,6 @@
 from radariq import RadarIQ
 
-# Initialize the RadarIQ sensor (replace 'COMx' with the appropriate COM port)
+# Initialize the RadarIQ sensor (replace 'COMx' with the appropriate COM port)/
 sensor = RadarIQ(port='COMx', output_format=RadarIQ.OUTPUT_LIST)
 
 # Start capturing data (continuous mode)
