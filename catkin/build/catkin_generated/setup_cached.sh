33b162415ce53ee83353123ee5a515c69c761dd7
warhammercasey <warhammercasey@gmail.com>
2023-10-03 08:51:55 -0600

    Created the Radar node with rospy dependency only

--- catkin/build/catkin_generated/setup_cached.sh
+++ catkin/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/strata/git/STRATA_Jetson/catkin/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/strata/git/STRATA_Jetson/catkin/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/melodic/bin:/home/strata/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/strata/git/STRATA_Jetson/catkin/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/strata/git/STRATA_Jetson/catkin/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/strata/git/STRATA_Jetson/catkin/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/strata/git/STRATA_Jetson/catkin/src:$ROS_PACKAGE_PATH"
