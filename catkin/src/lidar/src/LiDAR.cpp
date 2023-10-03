e07003ab34faf147a96ca82b3748eb62bf22b43c
warhammercasey <warhammercasey@gmail.com>
2023-10-03 09:32:32 -0600

    Added comments

--- catkin/src/lidar/src/LiDAR.cpp
+++ catkin/src/lidar/src/LiDAR.cpp
@@ -6,29 +6,39 @@
 #include "HPS3DUser_IF.h"
 
 
+// Define the frame buffer vector
+// Must be global due to being used in an interrupt
 typedef struct{
 	std::vector<HPS3D_PerPointCloudData_t> points;
 	uint16_t width;
 	uint16_t height;
 } PointCloudData_t;
 
-
 std::vector<PointCloudData_t> frameBuffer;
 
+// Global variable which stores the current measurement data
+// Must be global due to being used in an interrupt
 static HPS3D_MeasureData_t g_measureData;
 
 
+// LiDAR event ISR
+// I dont know anything about the parameters except that data is a pointer to a HPS3D_MeasureData_t struct
+// All this does is add the new data to the frame buffer and throws a warning if anything else happens
 static void EventCallBackFunc(int handle, int eventType, uint8_t *data,int dataLen, void *userPara);
 
 int main(int argc, char** argv){
+	// Initialize ros node
 	ros::init(argc, argv, "LiDAR");
-
 	ros::NodeHandle n;
 
+	// Initialize test publisher
 	ros::Publisher int_pub = n.advertise<std_msgs::Int32>("Chatter", 1000);
 
+	// Tell user were starting
 	ROS_INFO("Starting...");
 
+	// Start initializing the LiDAR
+	// Throw errors and return if initialization failed
 	int handle = -1;
 	HPS3D_StatusTypeDef ret = HPS3D_RET_OK;
 
@@ -63,11 +73,11 @@ int main(int argc, char** argv){
 	ROS_INFO("Connected!");
 	ROS_INFO("Max resolution: %dX\t%dY", settings.max_resolution_X, settings.max_resolution_Y);
 
+	// Start continuous capture
 	HPS3D_StartCapture(handle);
 
-
-	ros::Rate loop_rate(10);
-
+	// Run main loop at 20 Hz
+	ros::Rate loop_rate(40);
 
 	int count = 0;
 	while(ros::ok()){
@@ -82,6 +92,8 @@ int main(int argc, char** argv){
 		count++;
 		loop_rate.sleep();
 	}
+
+	// Cleanup ROS and LiDAR
 	ROS_INFO("Shutting down...");
 	HPS3D_StopCapture(handle);
 	HPS3D_CloseDevice(handle);
@@ -93,25 +105,33 @@ int main(int argc, char** argv){
 
 
 static void EventCallBackFunc(int handle, int eventType, uint8_t *data,int dataLen, void *userPara){
+	// Initialize point cloud structures
 	PointCloudData_t point_data;
-	HPS3D_PerPointCloudData_t* dataptr;
+	HPS3D_PerPointCloudData_t* dataptr; // This pointer is used to create the point_data.points vector from the raw LiDAR data
 
 	switch((HPS3D_EventType_t)eventType){
 
+		// This appears to be the only event callback we normally get. Use this tog et data
 		case HPS3D_FULL_DEPTH_EVEN:	
+			// Read the data into g_measureData
 			HPS3D_ConvertToMeasureData(data, &g_measureData, (HPS3D_EventType_t)eventType);
+
+			// Debug logging
 			ROS_INFO("Average distance: %d", g_measureData.full_depth_data.distance_average);
 			ROS_INFO("Width: %d\tHeight: %d\tPoints: %u", g_measureData.full_depth_data.point_cloud_data.width, g_measureData.full_depth_data.point_cloud_data.height, g_measureData.full_depth_data.point_cloud_data.points);
 
+			// Create the point_data struct
 			dataptr = g_measureData.full_depth_data.point_cloud_data.point_data;
 			point_data.points = std::vector<HPS3D_PerPointCloudData_t>(dataptr, dataptr + g_measureData.full_depth_data.point_cloud_data.points);
 			point_data.width = g_measureData.full_depth_data.point_cloud_data.width;
 			point_data.height = g_measureData.full_depth_data.point_cloud_data.height;
 			
+			// Add the data to the frame buffer
 			frameBuffer.push_back(point_data);
 			
 			break;
 
+		// These also contain data but they were never used in my tests so just leave them as a warning for now
 		case HPS3D_SIMPLE_ROI_EVEN:	
 		case HPS3D_FULL_ROI_EVEN:	
 		case HPS3D_SIMPLE_DEPTH_EVEN:
