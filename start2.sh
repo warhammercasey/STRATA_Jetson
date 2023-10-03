4332f17fccdfc569d45077208ba4bfe220daf05d
warhammercasey <warhammercasey@gmail.com>
2023-10-03 08:33:08 -0600

    Updated ip

--- start2.sh
+++ start2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/strata/git/startup/STRATA_Jetson
+sleep 10
 lanIp=$(ip -f inet addr show wlan0 | grep -oP 'inet \K[\d.]+')
 echo ${lanIp} > ip.txt
-git add ip.txt
+git add *
 git commit -m "Updated ip"
 git push -u origin ip
