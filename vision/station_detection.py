5971622310094510ebfb7ef4ba2f147eac387942
Nathan Wasniak <njwasniak@mines.edu>
2023-11-10 11:14:03 -0700

    Renamed images

--- vision/station_detection.py
+++ vision/station_detection.py
@@ -5,19 +5,24 @@
 import cv2
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 images = []
 numImages = 19
 
 # Create array of images to open
-
-
 for i in range(0,19):
     if i < 10:
         str = "0" + i
     else: # i >= 10
         str = i
-    images.append(cv2.imread('station'+str+'.jpg'))
+    img = cv2.imread('./vision/station'+str+'.jpg')
+    # Convert BGR to RGB (OpenCV uses BGR by default)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    images.append(img)
+
+fig, axes = plt.subplots(1, 3, figsize=(12, 4))
 
 for img in images:
     cv2.imshow(img)
+    
