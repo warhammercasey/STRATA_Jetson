28a9484020c605e25f83ef76fc8f4700ce51d151
warhammercasey <warhammercasey@gmail.com>
2023-11-27 18:45:14 -0700

    Improved LED detection

--- catkin/src/station_detection/StationDetection.py
+++ catkin/src/station_detection/StationDetection.py
@@ -8,12 +8,12 @@ import RPi.GPIO as GPIO
 from scipy.spatial.transform import Rotation as R
 import copy
 
-CIRCLE_THRESHOLD = 0.01 # How tolerant of things being circles. Higher values will detect more objects as circular
-MAX_CIRCLE_THRESHOLD = 0.3
-MORPH_KERNEL_SIZE = 6 # Kernel size for morphological opening
-MAX_MORPH_ITERATIONS = 1
-WHITE_THRESHOLD = 5
-MAX_WHITE_THRESHOLD = 50
+MORPH_KERNEL_SIZE = 20 # Kernel size for morphological opening
+
+CIRCLE_WEIGHT = 1.5
+WHITE_WEIGHT = 1
+DIST_WEIGHT = 0.5
+MIN_CONFIDENCE = (CIRCLE_WEIGHT + WHITE_WEIGHT + DIST_WEIGHT)*2/3
 
 DETECTION_COUNT = 10
 
@@ -145,10 +145,12 @@ def get_points(Ibgr, target_length, kernel_size = MORPH_KERNEL_SIZE, morph_itera
     I = cv2.cvtColor(Ibgr, cv2.COLOR_BGR2GRAY)
 
     # Threshold image
-    (thresh, im_bw) = cv2.threshold(I, np.max(I) - 50, 255, cv2.THRESH_BINARY)
+    (thresh, im_bw) = cv2.threshold(I, np.max(I) - 30, 255, cv2.THRESH_BINARY)
 
     # Morphologically open image to get rid of any noise
-    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    y, x = np.ogrid[-MORPH_KERNEL_SIZE/2:MORPH_KERNEL_SIZE/2+1, -MORPH_KERNEL_SIZE:MORPH_KERNEL_SIZE+1]
+    kernel = (x**2+y**2 <= (MORPH_KERNEL_SIZE/2)**2).astype(np.uint8)
+    #kernel = np.ones((kernel_size, kernel_size), np.uint8)
     im_bw = cv2.morphologyEx(im_bw, cv2.MORPH_OPEN, kernel, iterations=morph_iterations)
 
     # Calculated connected blobs
@@ -166,66 +168,70 @@ def get_points(Ibgr, target_length, kernel_size = MORPH_KERNEL_SIZE, morph_itera
         if kernel_size == 0:
             return ([], True)
         return get_points(Ibgr, target_length, kernel_size = kernel_size - 1, morph_iterations = morph_iterations)
-    '''elif num_labels > target_length + 1:
-        if morph_iterations >= MAX_MORPH_ITERATIONS:
-            return []
-        return get_points(img, target_length, kernel_size = kernel_size, morph_iterations = morph_iterations + 1)'''
-
-    # Figure out which blobs are circular
-    circular = [abs(((stats[i, cv2.CC_STAT_HEIGHT] + stats[i, cv2.CC_STAT_WIDTH])/4)**2*np.pi/stats[i, cv2.CC_STAT_AREA] - 1) <= CIRCLE_THRESHOLD for i in range(num_labels)]
-
-    # Increase thershold until enough circular blobs are detected
-    thresh = CIRCLE_THRESHOLD
-    while sum(circular) < target_length:
-        thresh += 0.01
-        if thresh > MAX_CIRCLE_THRESHOLD:
-            return ([], True)
-        circular = [abs(((stats[i, cv2.CC_STAT_HEIGHT] + stats[i, cv2.CC_STAT_WIDTH])/4)**2*np.pi/stats[i, cv2.CC_STAT_AREA] - 1) <= thresh for i in range(num_labels)]
-
     
-    # Get rid of any non-white blobs
-    white = circular[:]
-    for i in range(len(circular)):
-        if not circular[i]:
+
+    # Calculate the circularity of each blob based on https://sciencing.com/calculate-circularity-5138742.html
+    circular = np.zeros((num_labels))
+
+    for i in range(num_labels):
+        mask = np.array(labels == i+1, dtype='uint8')*255
+
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        if len(contours) <= 0:
             continue
-        
+
+        circular[i] = 4*np.pi*stats[i, cv2.CC_STAT_AREA]/(cv2.arcLength(contours[0], True)**2)
+
+    circular[circular > 1] = 0
+
+
+
+    # Calculate how white each blob is by converting to HSV color space
+    whiteness = np.zeros((num_labels))
+
+    hsv = cv2.cvtColor(Ibgr, cv2.COLOR_BGR2HSV)
+
+    for i in range(num_labels):
         pos = np.round(centroids[i]).astype(np.int32)
-        avg = np.mean(Ibgr[pos[1]][pos[0]])
         
-        white[i] = np.max(np.abs(np.array([avg, avg, avg]) - Ibgr[pos[1]][pos[0]])) <= WHITE_THRESHOLD
+        whiteness[i] = (255 - hsv[pos[1]][pos[0]][1])/255
+
+    # Find outliers by calculating the median x/y coordinate of the blobs and getting the distance to that median
+    distance = np.zeros((num_labels))
+    median_count = np.min([num_labels, np.round(target_length*1.5)]).astype(np.int32)
+
+    x_points = [centroids[x][0] for x in circular.argsort()[-median_count:]]
+    y_points = [centroids[x][1] for x in circular.argsort()[-median_count:]]
+
+    median_x = np.median(x_points)
+    median_y = np.median(y_points)
+
+    for i in range(num_labels):
+        distance[i] = np.sqrt((centroids[i][0] - median_x)**2 + (centroids[i][0] - median_y)**2)
+
+    distance = (np.max(distance) - distance)/np.max(distance)
+
+
+    # Give each blob a score based on circularity, whiteness, and distance from median
+    detected = circular*CIRCLE_WEIGHT + whiteness*WHITE_WEIGHT + distance*DIST_WEIGHT
+
+    # Calculate a confidence score based on the mean score of detected dots
+    confidence = np.mean(detected[detected.argsort()[-target_length:]])
+
+    # Get the centroids of the detected blobs
+    detected = centroids[detected.argsort()[-target_length:]]
     
-    # Increase white threshold until we have enough blobs detected
-    thresh = WHITE_THRESHOLD
-    while sum(white) < target_length:
-        thresh += 1
-        if thresh > MAX_WHITE_THRESHOLD:
-            print("Threshold not found")
-            return ([], True)
-        white = circular[:]
-        for i in range(len(circular)):
-            if not circular[i]:
-                continue
-            
-            pos = np.round(centroids[i]).astype(np.int32)
-            avg = np.mean(Ibgr[pos[1]][pos[0]])
-            
-            white[i] = np.max(np.abs(np.array([avg, avg, avg]) - Ibgr[pos[1]][pos[0]])) <= thresh
-        
-    circular = white[:]
-    
-    # Draw dots on each detected led
-    #for i in range(len(circular)):
-    #    if not circular[i]:
-    #        continue
-    #    
-    #    Ibgr = cv2.circle(Ibgr, (int(round(centroids[i][0])), int(round(centroids[i][1]))), radius=5, color=(0,0,255), thickness=-1)
-#
+    '''# Draw dots on each detected led
+    for i in detected:
+        Ibgr = cv2.circle(Ibgr, (int(np.round(i[0])), int(np.round(i[1]))), radius=5, color=(0,0,255), thickness=-1)
+
     ## Show images
-    #cv2.imshow("Base Image", Ibgr)
-    #cv2.imshow("Binary Image", im_bw)
-    #cv2.waitKey(0)
+    cv2.imshow("Base Image", Ibgr)
+    cv2.imshow("Binary Image", im_bw)
+    cv2.waitKey(0)'''
     
-    return (centroids[circular], False)
+    return (detected, False, confidence)
 
 def position_callback(data):
     global position
@@ -289,16 +295,19 @@ if __name__ == "__main__":
                 ret_val, frame = video_capture.read()
                 
                 # Calculate the pixel coordinates of all LEDs in the image
-                seen_points, failed = get_points(frame, len(target_points))
+                seen_points, failed, confidence = get_points(frame, len(target_points))
                 
-                for i in range(len(seen_points)):
-                    frame = cv2.circle(frame, (int(np.round(seen_points[i][0])), int(np.round(seen_points[i][1]))), 5, (0, 0, 255), -1)
-                
-                    frame = cv2.putText(frame, str(i), (int(seen_points[i][0]), int(seen_points[i][1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+                if SAVE_VIDEO:
+
+                    frame = cv2.putText(frame, "Confidence: " + str(confidence), (0, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+                    for i in range(len(seen_points)):
+                        frame = cv2.circle(frame, (int(np.round(seen_points[i][0])), int(np.round(seen_points[i][1]))), 5, (0, 0, 255), -1)
+                    
+                        frame = cv2.putText(frame, str(i), (int(seen_points[i][0]), int(seen_points[i][1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
                     
                 
                 # Check if detection failed
-                if (len(seen_points) != len(target_points)) or (failed):
+                if (len(seen_points) != len(target_points)) or (failed) or (confidence < MIN_CONFIDENCE):
                     detection_count = 0
                     rospy.loginfo("Detection failed")
 
@@ -330,7 +339,7 @@ if __name__ == "__main__":
                             rospy.loginfo("Final guidance: " + str(np.mean(position) - np.mean(initial_position)) + "\tPrevious distance: " + str(previous_distance[-1]))
 
                             # Check if we have docked or we moved too far
-                            if GPIO.input(STATION_PIN) or np.mean(position) - np.mean(initial_position) > previous_distance[-1]*1.25:
+                            if GPIO.input(STATION_PIN) or np.mean(position) - np.mean(initial_position) > previous_distance[-1]*1.2:
                                 # Disable final guidance
                                 final_guidance = False
 
@@ -480,15 +489,16 @@ if __name__ == "__main__":
 
                 #print((np.mean([i[0] for i in seen_points]) - H_RESOLUTION/2)/H_RESOLUTION*FOV)
                     
-                
-                frame = cv2.putText(frame, "Distance 1: " + str(distance), (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
-                
-                frame = cv2.putText(frame, "Rotation: " + str(rotation), (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
-                
-                if (not success[0]) and (not success[1]):
-                    frame = cv2.putText(frame, "FAILED", (0, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, 2)
-                
                 if SAVE_VIDEO:
+                    frame = cv2.putText(frame, "Distance 1: " + str(distance), (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+                    
+                    frame = cv2.putText(frame, "Rotation: " + str(rotation), (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+
+                    frame = cv2.putText(frame, "Reprojection Error: " + str(mean_error[min_idx]), (0, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+                    
+                    if (not success[0]) and (not success[1]):
+                        frame = cv2.putText(frame, "FAILED", (0, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, 2)
+                
                     out.write(frame)
         finally:
             video_capture.release()
--- vision/Reformat.py
+++ vision/Reformat.py
@@ -4,9 +4,14 @@ import numpy as np
 import itertools
 
 CIRCLE_THRESHOLD = 0.01 # How tolerant of things being circles. Higher values will detect more objects as circular
-MORPH_KERNEL_SIZE = 10 # Kernel size for morphological opening
-MAX_MORPH_ITERATIONS = 10
+MAX_CIRCLE_THRESHOLD = 0.3
+MORPH_KERNEL_SIZE = 20 # Kernel size for morphological opening
 WHITE_THRESHOLD = 5
+MAX_WHITE_THRESHOLD = 50
+
+CIRCLE_WEIGHT = 1.5
+WHITE_WEIGHT = 1
+DIST_WEIGHT = 0.5
 
 # Camera information
 FOV = 54
@@ -14,18 +19,22 @@ H_RESOLUTION = 640
 V_RESOLUTION = 480
 
 # Directory of images
-WORKING_DIR = "C:\\Users\\mmurr\\Documents\\School\\Senior Design\\Computer Vision"
+WORKING_DIR = "Z:\\disk2\\projects\\Senior Deisgn\\STRATA_Jetson\\vision"
 
 os.chdir(WORKING_DIR)
 
 
 # 3D points of LEDs
 target_points = np.array([
-    (18.98, 3.36, 0),
-    (48.76, 14.17, 0),
-    (40.36, 28.76, 0),
-    (33.61, 44.68, 0)
-], dtype="float64")
+    (0, 0, 0),
+    (100, 0, 0),
+    (50, 10, 0),
+    (75, 25, 0),
+    (90, 50, 0),
+    (50, 90, 0),
+    (0, 100, 0),
+    (100, 100, 0)
+], dtype='float32')
 
 #target_points = np.array([
 #    (0, 100, 0),
@@ -59,10 +68,12 @@ def get_points(Ibgr, target_length, kernel_size = MORPH_KERNEL_SIZE, morph_itera
     I = cv2.cvtColor(Ibgr, cv2.COLOR_BGR2GRAY)
 
     # Threshold image
-    (thresh, im_bw) = cv2.threshold(I, np.max(I) - 50, 255, cv2.THRESH_BINARY)
+    (thresh, im_bw) = cv2.threshold(I, np.max(I) - 30, 255, cv2.THRESH_BINARY)
 
     # Morphologically open image to get rid of any noise
-    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    y, x = np.ogrid[-MORPH_KERNEL_SIZE/2:MORPH_KERNEL_SIZE/2+1, -MORPH_KERNEL_SIZE:MORPH_KERNEL_SIZE+1]
+    kernel = (x**2+y**2 <= (MORPH_KERNEL_SIZE/2)**2).astype(np.uint8)
+    #kernel = np.ones((kernel_size, kernel_size), np.uint8)
     im_bw = cv2.morphologyEx(im_bw, cv2.MORPH_OPEN, kernel, iterations=morph_iterations)
 
     # Calculated connected blobs
@@ -78,90 +89,79 @@ def get_points(Ibgr, target_length, kernel_size = MORPH_KERNEL_SIZE, morph_itera
     # Dynamically reduce kernel size if we dont detect enough dots
     if num_labels < target_length:
         if kernel_size == 0:
-            return []
+            return ([], True)
         return get_points(Ibgr, target_length, kernel_size = kernel_size - 1, morph_iterations = morph_iterations)
-    '''elif num_labels > target_length + 1:
-        if morph_iterations >= MAX_MORPH_ITERATIONS:
-            return []
-        return get_points(img, target_length, kernel_size = kernel_size, morph_iterations = morph_iterations + 1)'''
-
-    # Figure out which blobs are circular
-    circular = [abs(((stats[i, cv2.CC_STAT_HEIGHT] + stats[i, cv2.CC_STAT_WIDTH])/4)**2*np.pi/stats[i, cv2.CC_STAT_AREA] - 1) <= CIRCLE_THRESHOLD for i in range(num_labels)]
-
-    # Increase thershold until enough circular blobs are detected
-    thresh = CIRCLE_THRESHOLD
-    while sum(circular) < target_length:
-        thresh += 0.01
-        if thresh > 1:
-            return []
-        circular = [abs(((stats[i, cv2.CC_STAT_HEIGHT] + stats[i, cv2.CC_STAT_WIDTH])/4)**2*np.pi/stats[i, cv2.CC_STAT_AREA] - 1) <= thresh for i in range(num_labels)]
-
     
-    # Get rid of any non-white blobs
-    white = circular[:]
-    for i in range(len(circular)):
-        if not circular[i]:
+
+    # Calculate the circularity of each blob based on https://sciencing.com/calculate-circularity-5138742.html
+    circular = np.zeros((num_labels))
+
+    for i in range(num_labels):
+        mask = np.array(labels == i+1, dtype='uint8')*255
+
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        if len(contours) <= 0:
             continue
-        
+
+        circular[i] = 4*np.pi*stats[i, cv2.CC_STAT_AREA]/(cv2.arcLength(contours[0], True)**2)
+
+    circular[circular > 1] = 0
+
+
+
+    # Calculate how white each blob is by converting to HSV color space
+    whiteness = np.zeros((num_labels))
+
+    hsv = cv2.cvtColor(Ibgr, cv2.COLOR_BGR2HSV)
+
+    for i in range(num_labels):
         pos = np.round(centroids[i]).astype(np.int32)
-        avg = np.mean(Ibgr[pos[1]][pos[0]])
         
-        white[i] = np.max(np.abs(np.array([avg, avg, avg]) - Ibgr[pos[1]][pos[0]])) <= WHITE_THRESHOLD
+        whiteness[i] = (255 - hsv[pos[1]][pos[0]][1])/255
+
+
+
+    # Find outliers by calculating the median x/y coordinate of the blobs and getting the distance to that median
+    distance = np.zeros((num_labels))
+    median_count = np.min([num_labels, np.round(target_length*1.5)]).astype(np.int32)
+
+    x_points = [centroids[x][0] for x in circular.argsort()[-median_count:]]
+    y_points = [centroids[x][1] for x in circular.argsort()[-median_count:]]
+
+    median_x = np.median(x_points)
+    median_y = np.median(y_points)
+
+    for i in range(num_labels):
+        distance[i] = np.sqrt((centroids[i][0] - median_x)**2 + (centroids[i][0] - median_y)**2)
+
+    distance = (np.max(distance) - distance)/np.max(distance)
+
+
+    # Give each blob a score based on circularity, whiteness, and distance from median
+    detected = circular*CIRCLE_WEIGHT + whiteness*WHITE_WEIGHT + distance*DIST_WEIGHT
+
+    # Calculate a confidence score based on the mean score of detected dots
+    confidence = np.mean(detected[detected.argsort()[-target_length:]])
+
+    # Get the centroids of the detected blobs
+    detected = centroids[detected.argsort()[-target_length:]]
     
-    # Increase white threshold until we have enough blobs detected
-    thresh = WHITE_THRESHOLD
-    while sum(white) < target_length:
-        thresh += 1
-        if thresh > 100:
-            print("Threshold not found")
-            return []
-        white = circular[:]
-        for i in range(len(circular)):
-            if not circular[i]:
-                continue
-            
-            pos = np.round(centroids[i]).astype(np.int32)
-            avg = np.mean(Ibgr[pos[1]][pos[0]])
-            
-            white[i] = np.max(np.abs(np.array([avg, avg, avg]) - Ibgr[pos[1]][pos[0]])) <= thresh
-        
-    circular = white[:]
-    
-    # Draw dots on each detected led
-    #for i in range(len(circular)):
-    #    if not circular[i]:
-    #        continue
-    #    
-    #    Ibgr = cv2.circle(Ibgr, (int(round(centroids[i][0])), int(round(centroids[i][1]))), radius=5, color=(0,0,255), thickness=-1)
-#
+    '''# Draw dots on each detected led
+    for i in detected:
+        Ibgr = cv2.circle(Ibgr, (int(np.round(i[0])), int(np.round(i[1]))), radius=5, color=(0,0,255), thickness=-1)
+
     ## Show images
-    #cv2.imshow("Base Image", Ibgr)
-    #cv2.imshow("Binary Image", im_bw)
-    #cv2.waitKey(0)
+    cv2.imshow("Base Image", Ibgr)
+    cv2.imshow("Binary Image", im_bw)
+    cv2.waitKey(0)'''
     
-    return centroids[circular]
+    return (detected, False, confidence)
 
-IMG = 'img5.jpg'
+IMG = 'Test images\\ImgTest1.png'
 
-seen_points = get_points(cv2.imread(IMG), len(target_points)).astype('float64')
+seen_points, failed, confidence = get_points(cv2.imread(IMG), len(target_points))
+
+if failed:
+    print("Failed")
   
-        
-success, best_vector, translation_vector = cv2.solvePnP(target_points, seen_points, calibration_matrix, np.zeros((4, 1)), flags=0)
-
-
-print("rotation_vector: " + str(best_vector[0]))
-print("translation_vector: " + str(best_vector[1]))
-
-I = cv2.imread(IMG)
-
-r = 5
-
-points = seen_points.astype('int')
-for i in range(len(points)):
-    I = cv2.circle(I, points[i], radius=r, color=(0,0,255), thickness=-1)
-    
-    I = cv2.putText(I, str(i), points[i], cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, 2)
-    
-cv2.imshow("Numbered", I)
-
-cv2.waitKey(0)
